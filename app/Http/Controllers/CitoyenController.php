1d5b77b4c67d0f50568becdf923a8886802dd18b
Hermance randria <hermancerandria@gmail.com>
2024-06-14 14:32:45 +0300

    traitement ajout

--- app/Http/Controllers/CitoyenController.php
+++ app/Http/Controllers/CitoyenController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CitoyenRequest;
+use App\Models\Citoyen;
 use Illuminate\Http\Request;
 
 class CitoyenController extends Controller
 {
     public function ajout_citoyen(){
         //ajout de citoyen miretourner interface formulaire
+        return view('citoyen.ajout');
     }
 
-    public function ajout_citoyen_traitement(){
+    public function ajout_citoyen_traitement(CitoyenRequest $request){
         //traitement ajout de citoyen
+        $citoyen = Citoyen::created($request->validated());
+        return redirect()->route("listeCitoyen")->with('Succée',"le citoyen a été bien enregistre");
+
     }
 
     public function liste_citoyen(){
         //liste de citoyen
+        $citoyens = Citoyen::all();
+        return view('citoyen.liste',[
+            'cytoyens' => $citoyens
+        ]);
     }
 }
--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ServiceRequest;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
     public function ajout_service(){
         //ajout service  miretourner interface formulaire
+        return view('servicee.ajout');//micreer dossier service de 
     }
 
-    public function ajout_service_traitement(){
+    public function ajout_service_traitement(ServiceRequest $request){
         //traitement ny formulaire ajout 
+        $service = Service::create($request->validated());
+        return redirect()->route('listeService')->with('Succée,la service a été bien enregistrer');
     }
 
     public function liste_service(){
         //liste nu service
+        $services = Service::all();
+        return view('servicee.liste',[
+            'services'=>$services
+        ]);
+        
     }
 
 }
