ea7b83e51ff076ad0f0f0daed46326b51685876e
Hermance randria <hermancerandria@gmail.com>
2024-06-14 11:37:42 +0300

    creation base de donnée

---
+++ database/migrations/2024_06_14_083245_create_citoyens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citoyens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomCitoyen');
+            $table->string('prenomCitoyen');
+            $table->string('email');
+            $table->string('numCIN')->unique();
+            $table->string('telephone');
+            $table->string('adresse');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citoyens');
+    }
+};
---
+++ database/migrations/2024_06_14_083311_create_souservices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('souservices', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_Souservice');
+            $table->foreignId('services_id')->constrained('services')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('souservices');
+    }
+};
